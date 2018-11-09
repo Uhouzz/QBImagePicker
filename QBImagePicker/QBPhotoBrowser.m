5aeb2d218845ab00e0b6439c655e4071812087e0
Tony <iOSBoy@163.com>
2018-11-09 16:47:20 +0800

    fix iPhoneXr issues

--- QBImagePicker/QBPhotoBrowser.m
+++ QBImagePicker/QBPhotoBrowser.m
@@ -318,7 +318,8 @@
 - (UIToolbar *)toolbar {
     if (!_toolbar) {
         CGFloat height = 44;
-        CGFloat safeAreaInsetsBottom = [UIScreen mainScreen].bounds.size.height == 812 ? 34 : 0;
+        BOOL needHair = ([UIScreen mainScreen].bounds.size.height == 812) || ([UIScreen mainScreen].bounds.size.height == 896);
+        CGFloat safeAreaInsetsBottom = needHair ? 34 : 0;
         _toolbar = [[UIToolbar alloc] initWithFrame:CGRectMake(0, self.view.bounds.size.height - height - safeAreaInsetsBottom, self.view.bounds.size.width, height)];
         if ([[UIToolbar class] respondsToSelector:@selector(appearance)]) {
             [_toolbar setBackgroundImage:nil forToolbarPosition:UIToolbarPositionAny barMetrics:UIBarMetricsDefault];
